ef58e6efd092f6996e04ec1dc2eea67e2f6d29cf
ctur <cturesoy@yahoo.com.tr>
2017-08-22 13:22:57 +0300

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,1 +1,116 @@
 // JS practice
+
+(function () {
+    var table = {
+        entries: [],
+        addEntry: function (key, value) {
+            this.entries.push({ key : key, value: value});
+        },
+        forEach: function (f, thisArg) {
+            var entries = this.entries;
+
+            for (var i = 0, n = entries.length; i < n; i++) {
+                var entry = entries[i];
+                f.call(thisArg, entry.key, entry.value, i);
+            }
+        }
+    };
+
+    var c3 = {
+        name: 'c',
+        printName: function () {
+            console.log(this.name);
+        }
+
+    };
+
+    var d3 = {
+        name: 'd'
+    };
+
+    function a(){
+        console.log(this.name);
+    };
+
+    c3.printName.call(d3);
+
+    var buffer = {
+        state: [],
+        append: function () {
+            for (var i = 0, n = arguments.length; i < n; i++) {
+                this.state.push(arguments[i]);
+            }
+        }
+    };
+
+    buffer.append("hello");
+    buffer.append.apply(buffer, ['a', 'b']);
+    console.log(buffer.state);
+
+    function averageOfArray(a) {
+        for (var i = 0, sum = 0, n = a.length; i < n; i++){
+            sum += a[i];
+        };
+        return sum / n;
+    }
+    console.log(averageOfArray([3, 7, 5, 2, 3, 9, 7, 6, 5]));
+
+    function average() {
+        var args = [].slice.call(arguments);
+        console.log('arrrgs: ' + args);
+        var shift = [].shift;
+        for (var i = 0, sum = 0, n = arguments.length;
+                i < n;
+                i++) {
+          
+            sum += arguments[i];
+            shift.call(arguments);
+            console.log(arguments);
+        }
+        
+        return sum / n;
+
+    };
+
+    console.log(average.apply(this, [3, 2, 1, 4]));
+
+    // Never modify arguments
+
+    // * The slice method of arrays makes a copy of an array when
+    // called without additional arguments. -> var args = [].slice.call(arguments);
+
+    function callMethod(obj, method) {
+        var args = [].slice.call(arguments, 2);
+        return obj[method].apply(obj, args);
+    }
+
+    var obj = {
+        add: function (x, y) { return x + y; }
+    };
+
+    console.log(callMethod(obj, "add", 17, 25)); // 42
+
+    var it = values(1, 4, 1, 4, 2, 1, 3, 5, 6);
+    console.log(it.next());
+    console.log(it.next());
+    console.log(it.next());
+    console.log(it.next());
+    console.log(it.next());
+
+    function value() {
+        var i = 0, n = arguments.length;
+        return {
+            hasNext: function () {
+                return i < n;
+            },
+            next: function () {
+                if (i >= n) {
+                    throw new Error("end of iteration");
+                }
+                return arguments[i++]; // wrong arguments
+            }
+        }
+
+    };
+
+})();

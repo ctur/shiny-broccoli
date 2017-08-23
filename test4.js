4282085e2d465032a30ac40f725ee1b881c4b751
ctur <cturesoy@yahoo.com.tr>
2017-08-23 10:51:34 +0300

    Update test4.js

--- test4.js
+++ test4.js
@@ -28,5 +28,39 @@
     console.log(top.children);
 
     // 37
+    // CSV - Comma Separated values
+    function CSVReader(separators) {
+        this.separators = separators || [","];
+        this.regexp =
+            new RegExp(this.separators.map(function (sep) {
+                return "\\" + sep[0];
+            }).join("|"));
+    }
+
+    CSVReader.prototype.read = function (str) {
+        var lines = str.trim().split(/\n/);
+        console.log(lines);
+        return lines.map(function (line) {
+            return line.split(this.regexp);
+        }, this); // forward out this-binding to callback
+    };
+
+    var reader = new CSVReader();
+    console.log(reader.read("a,b,c\nd,e,f\n"));
+
+    //  ->>> extra csv
+    CSVReader.prototype.read = function (str) {
+        var lines = str.trim().split(/\n/);
+        //***//
+        var self = this; // save a reference to outer this-binding
+        //***//
+
+        return lines.map(function (line) {
+            return line.split(self.regexp); // use outer this
+        });
+    };
+
+    var reader = new CSVReader();
+    console.log(reader.read("a,b,c\nd,e,f\n"));
 
 })();

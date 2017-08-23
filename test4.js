d9ba2f490efb519a14c67d397be04ac36703d9ff
ctur <cturesoy@yahoo.com.tr>
2017-08-23 11:45:33 +0300

    Update test4.js

--- test4.js
+++ test4.js
@@ -95,8 +95,22 @@
         this.scene = scene;
         this.x = x;
         this.y = y;
+        this.actorID = ++Actor.nextID;
         scene.register(this);
     };
+
+    Actor.nextID = 0;
+
+    function Alien(scene, x, y, direction, speed, strength) {
+        Actor.call(this, scene, x, y);
+        this.direction = direction;
+        this.speed = speed;
+        this.strength = strength;
+        this.damage = 0;
+        this.alienID = ++Alien.nextID;
+    };
+
+    Alien.nextID = 0;
 
     Actor.prototype.moveTo = function (x, y) {
         this.x = x;

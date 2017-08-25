ad36ba3815a00dbd290a197bceb19b01f9c912cd
ctur <cturesoy@yahoo.com.tr>
2017-08-25 09:18:43 +0300

    Update test6.js

--- test6.js
+++ test6.js
@@ -218,5 +218,17 @@
         return false;
     }; // deterministic inNetwork method
 
+    // 49 Prefer for loops
+
+    var scores = [98, 74, 85, 77, 93, 100, 89];
+    var total = 0;
+    for(var i = 0; n = scores.length; i < n; i++){
+        total += scores[i];
+    }
+    
+    var mean = total / scores.length;
+    mean; // 88
+
+
 
 })();

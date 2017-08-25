bdd43adfd4e56310c059f5a766e1bd97fdedb158
ctur <cturesoy@yahoo.com.tr>
2017-08-25 15:34:57 +0300

    Update test7.js

--- test7.js
+++ test7.js
@@ -2,7 +2,21 @@
 
 (function () {
     // 51 Reuse generic array methods
+    function Highlight() {
+        [].forEach.call(arguments, function (widget) {
+            widget.setBackground("yellow");
+        });
+    }
 
+    var arrayLike = { 0: "a", 1: "b", 2: "c", length: 3 };
+    var result = Array.prototype.map.call(arrayLike, function (s) {
+        return s.toUpperCase();
+    });
+    console.log(result);
+
+    var result = Array.prototype.map.call("abc", function (s) {
+        return s.toUpperCase();
+    });
 
 
 })();
